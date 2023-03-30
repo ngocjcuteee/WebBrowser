652ced60606235b8e2ba6d7310a257691657331a
blee203 <bichlybichly.0@gmail.com>
2023-03-30 22:31:39 +0700

    change program.cs

--- FireFux/Program.cs
+++ FireFux/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FireFux
+namespace Bee
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace FireFux
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Bee());
         }
     }
 }
