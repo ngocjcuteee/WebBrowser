0d7e8e0aa7d865b5cee76d22fb08a503278ec68b
blee203 <bichlybichly.0@gmail.com>
2023-03-31 00:09:54 +0700

    change Bee.cs

--- FireFux/Bee.cs
+++ FireFux/Bee.cs
@@ -50,32 +50,10 @@ namespace Bee
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            webBrowser1 = new WebBrowser();
             webBrowser1.Size = tabControl1.Size;
+            webBrowser1.Navigate("https://www.google.com");
             webBrowser1.DocumentCompleted += webBrowser1_DocumentCompleted;
-            webBrowser1.ScriptErrorsSuppressed = true;
-            webBrowser1.Dock = DockStyle.Fill;
-            webBrowser1.Visible = true;
-            webBrowser1.Navigate("http://" + "google.com");
-            toolStripTextBox1.Text = "http://" + "google.com";
-            string title = this.webBrowser1.DocumentTitle;
-            tabControl1.Dock = DockStyle.Fill;
-            if (i != 0)
-                tabControl1.SelectTab(i - 1);
-            tabControl1.TabPages[0].Text = title;
-            tabControl1.SelectedTab.Controls.Add(webBrowser1);
-            tabControl1.Size = webBrowser1.Size;
-
-            //khởi tạo giá trị cho mảng đánh đấu
-            /*for (int y = 0; y < 100; y++)
-            {
-                danhDauTrang[y] = new Class_DanhDau();
-                danhDauTrang[y].LuuDauTrang = ".";
-                danhDauTrang[y].LuuTieuDe = ".";
-            }*/
-            //timer1.Start();
-
-
+            toolStripTextBox1.Text= "https://" + "google.com";
         }
         private void Form1_Resize(object sender, EventArgs e)
         {
@@ -91,17 +69,17 @@ namespace Bee
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-
+            tabControl1.SelectedTab.Text = webBrowser1.DocumentTitle;
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
-
+            webBrowser1.GoBack();
         }
 
         private void toolStripSplitButton2_ButtonClick(object sender, EventArgs e)
         {
-
+            webBrowser1.GoForward();
         }
         // Xóa hết tất cả các tab hiện có và tạo 1 tab mới
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
@@ -115,6 +93,8 @@ namespace Bee
             webBrowser1.ScriptErrorsSuppressed = true;
             webBrowser1.Dock = DockStyle.Fill;
             webBrowser1.Visible = true;
+            webBrowser1.Navigate("http://" + "google.com");
+            toolStripTextBox1.Text = "http://" + "google.com";
             tabControl1.TabPages.Add("+");
             tabControl1.SelectTab(i);
             tabControl1.SelectedTab.Tag = i.ToString();
@@ -122,5 +102,30 @@ namespace Bee
             i++;
         }
 
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void toolStripTextBox1_Click(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+                if (web != null)
+                {
+                    web.Navigate(toolStripTextBox1.Text);
+                }
+            }
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
